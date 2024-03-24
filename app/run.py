072b80a74444b362e6c16669fcf12845e49b3be5
95460eza <alfredkoffisokpa@gmail.com>
2024-03-24 13:07:40 -0400

    Removed flask_web_app.run()

--- app/run.py
+++ app/run.py
@@ -3,5 +3,5 @@ from Flask_webapp import flask_web_app
 
 
 
-if __name__ == '__main__':
-    flask_web_app.run(host='0.0.0.0', port=8000, debug=True)
+#if __name__ == '__main__':
+#    flask_web_app.run(host='0.0.0.0', port=8000, debug=True)
